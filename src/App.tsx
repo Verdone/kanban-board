deacee2c43c1aca140776563a792c99bb43d0747
Verdone <giulianoverdone@gmail.com>
2023-10-23 17:34:33 -0400

    initial commit
    create React app with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  const title = "Do market research";
+  const id = "BUS-1";
+  const points = 5;
+
+  return (
+    <div className="border rounded-lg px-2 m-2 bg-gray-50">
+      <div className="text-4xl py-2">{title}</div>
+      <div className="flex gap-4 justify-between py-2 text-gray-700">
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
